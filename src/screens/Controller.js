3504234000265808e94f1c272a65a91b7965b612
Zeelani <er.zeelumak@gmail.com>
2021-06-23 00:29:44 +0530

    Created gridlayout to display the cards in the home page

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,21 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Home from '../screens/home/Home';
 import Login from '../screens/login/Login';
+import Profile from '../screens/profile/Profile';
 
-import {BrowserRouter as Router,Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class Controller extends Component {
 
     constructor() {
         super();
-        this.baseurl="http://localhost:8080/";
     }
     render() {
         return (
             <Router>
                 <div className="main-controller">
-                    <Route exact path='/' render={(props) => <Login {...props} baseurl = {this.baseurl}/>} />
-                    <Route exact path='/home' render={(props) => <Home {...props} baseurl = {this.baseurl}/>} />
+                    <Route exact path='/' render={({ history }, props) => <Login {...props} history={history} />} />
+                    <Route exact path='/home' render={({ history }, props) => <Home {...props} history={history} />} />
+                    <Route exact path='/profile' render={({ history }, props) => <Profile {...props} history={history} />} />
                 </div>
             </Router>
         )
